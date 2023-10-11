f0d176d1d19f20ea06b4d244310fb8188412307c
Emmanuellaodunsi <145551431+Emmanuellaodunsi@users.noreply.github.com>
2023-10-11 23:19:31 +0100

    tuple_method_use_cases

---
+++ 2_use_cases_method_tuple.py
@@ -0,0 +1,15 @@
+"""2 use cases in tuple"""
+#Getting the maximum and minimum value from a list of numbers
+"""using max() and min() method"""
+numbers=(5,2,1,7,9,75,6)
+maximum=max(numbers)
+minimum=min(numbers)
+print("The maximum values in the numbers is:",maximum)
+print("The minimum values in the numbers is:",minimum)
+
+#Calculating the sum of all elements
+"""using sum()method""" 
+Numbers=(6,8,9,4,6,2,5)
+total=sum(Numbers)
+print("The total sum of the values is:\n",total)
+
